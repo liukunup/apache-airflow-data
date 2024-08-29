d9342f436ee1e5f18581549bb75097821b53b4e2
刘坤 <liukunup@163.com>
2024-08-29 02:52:54 +0000

    补充镜像预拉取脚本

---
+++ deploy/pull_images.sh
@@ -0,0 +1,5 @@
+docker pull docker.io/apache/airflow:2.9.3
+docker pull docker.io/library/redis:7.2-bookworm
+docker pull docker.io/bitnami/postgresql:16.1.0-debian-11-r15
+docker pull quay.io/prometheus/statsd-exporter:v0.26.1
+# docker pull registry.k8s.io/git-sync/git-sync
